1533a4eb8ebd843b7bd1247dfc55d4b5a3e8ad13
null <dwsong07@naver.com>
2021-09-03 10:07:40 +0900

    review

--- guide/.vuepress/components/ResultingCode.vue
+++ guide/.vuepress/components/ResultingCode.vue
@@ -1,19 +1,16 @@
 <template>
 	<p>
-		지금까지 만들어온 코드를 비교해보고 싶으시다면, 깃헙 리포지토리에서
-		확인하실 수 있습니다.
-		<a :href="githubLink" target="_blank" rel="noopener noreferrer">
-			깃헙 <OutboundLink />
-		</a>
+		지금까지 만들어온 코드를 비교해보고 싶으시다면,
+		<a :href="githubLink" target="_blank" rel="noopener noreferrer">이 깃헙 리포지토리 <OutboundLink /></a>
+		에서 확인하실 수 있습니다.
 	</p>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { usePageData } from "@vuepress/client";
+import { onMounted, ref } from 'vue';
+import { usePageData } from '@vuepress/client';
 
-const codeSamplesURL =
-	"https://github.com/discordjs/guide/tree/main/code-samples/";
+const codeSamplesURL = 'https://github.com/discordjs/guide/tree/main/code-samples/';
 
 const props = defineProps({
 	path: String,
@@ -23,7 +20,6 @@ const githubLink = ref(codeSamplesURL);
 const page = usePageData();
 
 onMounted(() => {
-	githubLink.value +=
-		props.path || page?.value.path.slice(1).replace(".html", "");
+	githubLink.value += props.path || page?.value.path.slice(1).replace('.html', '');
 });
 </script>
